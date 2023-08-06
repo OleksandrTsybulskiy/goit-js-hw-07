8fb6379151fff92a91e23399d0bd746ac042462b
OleksandrTsybulskiy <126475884+OleksandrTsybulskiy@users.noreply.github.com>
2023-08-06 21:15:11 +0300

    hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+let instance;
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join("");
+};
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+container.addEventListener("click", handlerGalleryClick);
+
+function handlerGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgLarge = evt.target.dataset.source;
+
+  instance = basicLightbox.create(`<img src="${imgLarge}" />`, {
+    onShow: (instance) => {
+      document.addEventListener("keydown", onKeyDown);
+    },
+    onClose: (instance) => {
+      document.removeEventListener("keydown", onKeyDown);
+    },
+  });
+
+  instance.show();
+}
+
+function onKeyDown(evt) {
+  if (evt.code === "Escape") {
+    evt.preventDefault();
+    
+    if (instance) {
+      instance.close();
+    }
+  }
+};
---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+let lightbox;
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('')
+};
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+container.addEventListener('click', handlerGalleryClick);
+
+function handlerGalleryClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+};
+
+lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
